1be021de541378f3a3c16dd6787ea7dac0f43811
Pranjal Soni <pranjal@miclient.ai>
2026-03-31 18:31:32 +0530

    update

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -13,11 +13,18 @@ router.post("/register", async (req, res) => {
         .status(400)
         .json({ message: "Username , email and password is required!" });
     }
+    const isEmail = username.includes("@");
 
-    const existingUSer = await pool.query(
-      `SELECT id FROM users WHERE username=$1`,
-      [username]
-    );
+    let existingUSer;
+
+    if (isEmail) {
+      existingUSer = await pool.query(
+        `SELECT id FROM users WHERE email=$1`,
+        [email],
+      );
+    } else {
+      existingUSer = await pool.query(`SELECT id FROM users WHERE username=$1`, [username]);
+    }
 
     if (existingUSer.rows.length) {
       return res.status(409).json({ message: "User already exists!" });
@@ -27,7 +34,7 @@ router.post("/register", async (req, res) => {
 
     const result = await pool.query(
       `INSERT INTO users(email, username, password) VALUES($1,$2,$3) RETURNING id,username`,
-      [email, username, hashedPassword]
+      [email, username, hashedPassword],
     );
 
     const user = result.rows[0];
@@ -35,7 +42,7 @@ router.post("/register", async (req, res) => {
     const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
       expiresIn: "1d",
     });
-    res.json({ success: true, token: token, userId: user.id});
+    res.json({ success: true, token: token, userId: user.id });
   } catch (err) {
     console.log(err);
     res.sendStatus(501);
@@ -47,12 +54,14 @@ router.post("/forgot_password", async (req, res) => {
   try {
     const { username, password } = req.body;
     if (!username || !password) {
-      return res.status(400).json({ message: "Username and password are required!" });
+      return res
+        .status(400)
+        .json({ message: "Username and password are required!" });
     }
 
     const existingUser = await pool.query(
       `SELECT id FROM users WHERE username=$1`,
-      [username]
+      [username],
     );
 
     if (!existingUser.rows.length) {
@@ -64,12 +73,11 @@ router.post("/forgot_password", async (req, res) => {
 
     await pool.query(
       `UPDATE users SET password = $1 WHERE id = $2 RETURNING id, username`,
-      [hashedPassword, userId]
+      [hashedPassword, userId],
     );
 
     // Use return here to stop the function
     return res.json({ success: true, message: "Password updated!" });
-
   } catch (err) {
     console.log(err);
     // Use return here too
@@ -80,8 +88,10 @@ router.post("/forgot_password", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const result = await pool.query(`SELECT * FROM users WHERE username=$1`,[username]);
-    const user = result.rows[0]
+    const result = await pool.query(`SELECT * FROM users WHERE username=$1`, [
+      username,
+    ]);
+    const user = result.rows[0];
 
     if (!user) {
       return res.status(401).send({ message: "User not found!!" });
@@ -96,7 +106,7 @@ router.post("/login", async (req, res) => {
     const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, {
       expiresIn: "1d",
     });
-    res.json({ success: "true", token,  userId: user.id});
+    res.json({ success: "true", token, userId: user.id });
   } catch (err) {
     console.log(err.message);
     res.sendStatus(501);
