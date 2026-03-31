8bbb3ec3e9d819fd00c9b5bae24106a9c63788a2
Pranjal Soni <pranjal@miclient.ai>
2026-03-31 17:23:44 +0530

    live url added

--- src/server.js
+++ src/server.js
@@ -16,7 +16,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://urban-market-steel.vercel.app/",
+    ],
     credentials: true,
   }),
 );
