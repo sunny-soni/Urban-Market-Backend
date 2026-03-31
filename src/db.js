4625327c64504a25813721d015b264c02b4f3e3c
Pranjal Soni <pranjal@miclient.ai>
2026-03-31 17:53:44 +0530

    update

--- src/db.js
+++ src/db.js
@@ -1,11 +1,21 @@
 import pkg from "pg";
-
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "pranjal",
-  database: "invoicing",
-  port: 5432,
-});
+const isProduction = process.env.NODE_ENV === "production";
+
+export const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL, // Supabase (Render)
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        host: "localhost",
+        user: "postgres",
+        password: "pranjal",
+        database: "invoicing",
+        port: 5432,
+      }
+);
